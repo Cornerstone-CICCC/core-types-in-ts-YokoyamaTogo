6c8d55d5c64f123bbd637c6e8948aa57a89953db
kirakira <15catinthebox@gmail.com>
2026-04-15 20:56:59 -0700

    ex02

---
+++ dist/ex02.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fruits = ["apple", "banana", "orange"];
+fruits.push("mango");
+console.log(fruits);
+//# sourceMappingURL=ex02.js.map
